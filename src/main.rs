47b65cb12ac87d9cc6ab1a8fdd0c0202ea686e65
talhadaar <muhammadtalhadar@gmail.com>
2024-02-02 11:11:33 +0500

    reworking some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,50 @@
-use anyhow::{bail, Result};
 use clap::Parser;
 use std::{
     fs::File,
     io::{BufRead, BufReader, BufWriter, Write},
     path::PathBuf,
 };
-use thiserror::Error;
 
-// CLI args parser
-#[derive(Parser, Debug)]
-struct Args {
-    // Csv file read path
-    #[arg(short, long)]
-    read_path: PathBuf,
-    // Output data to new csv file or update existing one
-    #[arg(short, long)]
-    write_path: Option<PathBuf>,
-    // Sub command for handling data in csv file
-    #[clap(subcommand)]
-    command: Command,
-}
+pub mod command;
+use command::*;
 
-// Command and args prser
-#[derive(Parser, Debug)]
-enum Command {
-    // Display entire file
-    Display,
-    // Modify a row/field
-    Modify {
-        #[clap(short, long)]
-        row: usize,
-
-        #[clap(short, long)]
-        col: usize,
-
-        // NOTE: Vec<String> is for convenience only, please expect a Vec of len 1 only.
-        #[clap(short, long, value_delimiter = ',')]
-        data: Vec<String>,
-    },
-}
-
-//Custom errors
-#[derive(Error, Debug)]
-enum Error {
-    #[error("Some custom errors")]
-    SomeErrors,
+// Some custom errors yo you will implement
+#[derive(Debug)]
+enum CSVError {
+    SomeError = 0,
 }
 
 #[allow(dead_code)]
 #[derive(Debug, Default)]
-struct CSVFile {
+struct CSVData {
     data: Vec<Vec<String>>,
     rows: usize,
     cols: usize,
 }
 
-impl CSVFile {
+impl CSVData {
     // TODO read data into CSV struct
-    pub fn from_file(file_path: PathBuf) -> Result<Self> {
-        Ok(Self::default())
+    pub fn read(file_path: PathBuf) -> Result<(), CSVError> {
+        Ok(())
     }
 
     // TODO Either update the existing csv file or create a new one - your choice
-    fn to_file(&self, file_path: PathBuf) -> Result<()> {
+    pub fn write(&self, file_path: PathBuf) -> Result<(), CSVError> {
         Ok(())
     }
+
+    // TODO display this file
+    pub fn display(&self) {}
 }
 
 fn main() {
-    // TODO Read and parse CLI args
+    // Reading CLI args and file
     let args = Args::parse();
+    let mut csv_data = CSVData::read(args.read_path).unwrap();
 
-    // TODO read CSV file
-    let mut csv_data = CSVFile::from_file(args.read_path).unwrap();
-
-    // TODO parse
+    // match and execute command
     match args.command {
         Command::Display => println!("Prints the CSV data"),
-        // NOTE: Vec<String> is for convenience only, please expect a Vec of len 1 only.
         Command::Modify { row, col, data } => println!("Modifies the CSV file"),
     }
 }
