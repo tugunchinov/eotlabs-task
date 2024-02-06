1e359513597a6623b5468c95735521437afb1590
talhadaar <muhammadtalhadar@gmail.com>
2024-02-06 14:51:19 +0500

    finalize this

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-//! # Requirements
-//! display: Prints out the csv struct
-//! replace: Replaces an item in the csv struct by another at any given coordinates
-//!
-//! # Some other Requirements
-//! Please utilize the following in your solution:- Associate Type, Trait, Trait Constraint or where clause, and Result<>
-//! Custom error variants and handle them where possible.
-//! Please pay attention to code readability and structure - separate into multiple files if possible.
-//!
-//! ## NOTE We are looking for a creative implementation, template provided is just a reference point
-//! you can modify it as you wish, except for the CLI commands part.
-//! 
-//! ## NOTE
-//! Use only std and the crates provided
-//!
-//! # Demo Commands
-//!
-//! ## Display
-//! cargo run -- --read-path=./data.csv display
-//!
-//! ## Replace
-//! cargo run -- --read-path=./data.csv replace -r 1 -c 1 -d yolo
-
 use clap::Parser;
 use std::{
     fs::File,
