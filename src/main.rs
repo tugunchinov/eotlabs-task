6cb07a18ac3e7bdeea9e3ff5309d74aeed1e5fb6
talhadaar <muhammadtalhadar@gmail.com>
2024-02-19 10:17:12 +0500

    helper code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::{
     fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{BufRead, BufReader, Error},
     path::PathBuf,
 };
 
@@ -43,29 +43,32 @@ enum CSVError {
 
 #[allow(dead_code)]
 #[derive(Debug, Default)]
-struct CSVData {
+struct CSVFile {
     data: Vec<Vec<String>>,
     rows: usize,
     cols: usize,
 }
 
-impl CSVData {
-    // TODO read data into CSV struct
-    pub fn read(&self, file_path: PathBuf) {
-        println!("TODO: Reads CSV file into CSV struct")
-    }
+pub trait CSVFileReader {
+    fn read(&mut self, file_path: PathBuf) -> Result<(), Error>;
+}
 
-    // TODO replaces an item in give coordinates
-    pub fn replace(&self, row: usize, col: usize, data: String) {
-        println!(
-            "TODO: Replace item at coords ({:?},{:?}) by ({:?})",
-            row, col, data
-        )
-    }
+impl CSVFileReader for CSVFile {
+    fn read(&mut self, file_path: PathBuf) -> Result<(), Error> {
+        let file = File::open(file_path)?;
+        let buff = BufReader::new(file);
 
-    // TODO display this file
-    pub fn display(&self) {
-        println!("TODO: Prints the CSV data")
+        for (index, line) in buff.lines().enumerate() {
+            let line = line?;
+            let row: Vec<String> = line.split(',').map(|s| s.trim().to_string()).collect();
+            if index == 0 {
+                self.cols = row.len();
+            }
+            self.data.push(row);
+            self.rows += 1
+        }
+
+        Ok(())
     }
 }
 
@@ -73,16 +76,13 @@ fn main() {
     // Reading CLI args
     let args = Args::parse();
 
-    // create CSVData instance and read file into it
-    let mut csv = CSVData::default();
-    csv.read(args.read_path);
+    // create CSVFile instance and read file into it
+    let mut csv = CSVFile::default();
+    let _ = csv.read(args.read_path);
 
     // match and execute command
     match args.command {
-        Command::Display => csv.display(),
-        Command::Replace { row, col, data } => {
-            csv.replace(row, col, data);
-            csv.display()
-        }
+        Command::Display => println!("--Display CSVFile--"),
+        Command::Replace { row, col, data } => println!("--Replace and write to file--"),
     }
 }
